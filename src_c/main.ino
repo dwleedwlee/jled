83565b51208e25c7d586c00703f2308d61373e2c
Dongwook Lee <elight.lee@gmail.com>
2022-08-21 12:51:25 +0900

    Update the .ino example

--- src_c/main.ino
+++ src_c/main.ino
@@ -5,8 +5,8 @@ void setup() {
   // breathe LED for 5 times
   JLED_DelayAfter(JLED_Repeat(JLED_Breathe(JLED_Init(JLED_GetStaticJledFromIndex(JLED_RES_LED0), HalPwm_GetStaticFromIndex(HAL_PWM_RES_LED0)), 2000), 5), 2000);
   JLED_DelayAfter(JLED_Repeat(JLED_Blink(JLED_Init(JLED_GetStaticJledFromIndex(JLED_RES_LED1), HalPwm_GetStaticFromIndex(HAL_PWM_RES_LED1)), 1000, 1000), 5), 2000);
-  JLED_DelayAfter(JLED_Repeat(JLED_Candle(JLED_Init(JLED_GetStaticJledFromIndex(JLED_RES_LED2), HalPwm_GetStaticFromIndex(HAL_PWM_RES_LED2)), 6, 15, 2000), 5), 2000);
-  JLED_Repeat(JLED_Blink(JLED_Init(JLED_GetStaticJledFromIndex(JLED_RES_LED3), HalPwm_GetStaticFromIndex(HAL_PWM_RES_LED3)), 500, 1500), 5), 2000;
+  JLED_DelayAfter(JLED_Forever(JLED_Candle(JLED_Init(JLED_GetStaticJledFromIndex(JLED_RES_LED2), HalPwm_GetStaticFromIndex(HAL_PWM_RES_LED2)), 6, 15, 2000)), 2000);
+  JLED_Forever(JLED_Blink(JLED_Init(JLED_GetStaticJledFromIndex(JLED_RES_LED3), HalPwm_GetStaticFromIndex(HAL_PWM_RES_LED3)), 500, 1500)), 2000;
 
   JLED_SequenceInit(JLED_GetStaticJledSeqFromIndex(JLED_SEQ_RES_LED), JLED_SEQ_PARALLEL, JLED_GetStaticJledFromIndex(JLED_RES_LED0), JLED_RES_MAX);
 }
